63675fae7d64ce1ab39cc65e7cfc5c063e4eedd7
Jan Goessens <jangoessens@hotmail.com>
2018-11-11 17:07:24 +0100

    update PasDag time + fall down on countdown

--- src/components/PasDag.js
+++ src/components/PasDag.js
@@ -2,6 +2,7 @@ import React from 'react';
 import DonationButton from './DonationButton';
 import CountdownClock from './CountdownClock';
 import BackgroundBubble from './Beer-Background/BackgroundBubble';
+import WaarIsDiePas from './WaarIsDiePas';
 
 const generatedBubbles = [];
 generateBubbles();
@@ -12,9 +13,7 @@ function generateBubbles() {
 };
 
 function generateBubble() {
-    var bubbleSize = Math.floor(Math.random() * 30);
-    var xpos = Math.floor((Math.random() * 100) - 4);
-    return <BackgroundBubble bubbleSize={bubbleSize} xpos={xpos}></BackgroundBubble>
+    return <BackgroundBubble></BackgroundBubble>
 }
 const PasDag = props => (
     <div className="PasDag-container">
@@ -23,10 +22,38 @@ const PasDag = props => (
                 <div className="beer-foam shake-after-fall-down">
                     {generatedBubbles}
                 </div>
-                <DonationButton></DonationButton>
+                <WaarIsDiePas></WaarIsDiePas>
+
+                <div className="bubble" style={{ left: 20 + 'vw', animationDelay: 1.4 + "s" }}></div>
+                <div className="bubble" style={{ left: 30 + 'vw', animationDelay: 2 + "s"}} ></div>
+                <div className="bubble" style={{ left: 40 + 'vw', animationDelay: 1.3 + "s"}} ></div>
+                <div className="bubble" style={{ left: 50 + 'vw', animationDelay: 2.1 + "s"}} ></div>
+                <div className="bubble" style={{ left: 20 + 'vw', animationDelay: 2 + "s"}} ></div>
+                <div className="bubble" style={{ left: 30 + 'vw', animationDelay: 2 + "s"}} ></div>
+                <div className="bubble" style={{ left: 40 + 'vw', animationDelay: 1.5 + "s"}} ></div>
+                <div className="bubble" style={{ left: 50 + 'vw', animationDelay: 3.7 + "s"}} ></div>
+                <div className="bubble" style={{ left: 35 + 'vw', animationDelay: .8 + "s"}} ></div>
+                <div className="bubble" style={{ left: 27 + 'vw', animationDelay: .6 + "s"}} ></div>
+                <div className="bubble" style={{ left: 32 + 'vw', animationDelay: 4.2 + "s"}} ></div>
+                <div className="bubble" style={{ left: 10 + 'vw', animationDelay: 2.5 + "s"}} ></div>
+                <div className="bubble" style={{ left: 26 + 'vw', animationDelay: .5 + "s"}}></div>
+                <div className="bubble" style={{ left: 70 + 'vw', animationDelay: 1.4 + "s" }}></div>
+                <div className="bubble" style={{ left: 89 + 'vw', animationDelay: 2 + "s" }} ></div>
+                <div className="bubble" style={{ left: 90 + 'vw', animationDelay: 1.3 + "s" }} ></div>
+                <div className="bubble" style={{ left: 67 + 'vw', animationDelay: 2.1 + "s" }} ></div>
+                <div className="bubble" style={{ left: 86 + 'vw', animationDelay: 2 + "s" }} ></div>
+                <div className="bubble" style={{ left: 14 + 'vw', animationDelay: 2 + "s" }} ></div>
+                <div className="bubble" style={{ left: 4 + 'vw', animationDelay: 1.5 + "s" }} ></div>
+                <div className="bubble" style={{ left: 20 + 'vw', animationDelay: 3.7 + "s" }} ></div>
+                <div className="bubble" style={{ left: 35 + 'vw', animationDelay: .8 + "s" }} ></div>
+                <div className="bubble" style={{ left: 67 + 'vw', animationDelay: .6 + "s" }} ></div>
+                <div className="bubble" style={{ left: 78 + 'vw', animationDelay: 4.2 + "s" }} ></div>
+                <div className="bubble" style={{ left: 30 + 'vw', animationDelay: 2.5 + "s" }} ></div>
+                <div className="bubble" style={{ left: 16 + 'vw', animationDelay: .5 + "s" }}></div>
+
+                        < DonationButton ></DonationButton>
 
             </div>
-
         </div>
         <CountdownClock timeUntillPasDag={0} appendClass="fall-down"></CountdownClock>
     </div>
